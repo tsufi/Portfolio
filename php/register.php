47fe98d257c4759ce89ea3b3eb87b186fe2a6e4a
tsufi <jydi93@gmail.com>
2025-03-25 12:23:56 +0200

    loginfix

--- php/register.php
+++ php/register.php
@@ -16,11 +16,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->close();
 }
 ?>
-
-<form action="register.php" method="post">
-    <label>Username:</label>
-    <input type="text" name="username" required><br>
-    <label>Password:</label>
-    <input type="password" name="password" required><br>
-    <button type="submit">Register</button>
-</form>
